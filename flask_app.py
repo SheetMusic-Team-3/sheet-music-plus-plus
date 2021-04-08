12d04ddfb57d5c849425dd236288b90f5ce51d4d
hilnels <hilnels@8e86bfb0e784>
2021-04-08 20:53:55 +0000

    added app routes

--- flask_app.py
+++ flask_app.py
@@ -151,6 +151,17 @@ def download():
     global PATH
     return send_file(PATH, as_attachment=True)
 
+@app.route("/about")
+def about():
+
+    return render_template('about.html')
+
+
+@app.route("/help")
+def help():
+
+    return render_template('help.html')
+
 
 if __name__ == "__main__":
     app.run()
