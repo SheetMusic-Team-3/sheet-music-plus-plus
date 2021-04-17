8dbaa2d18660b28ce836853c723f6626a073f5b6
James Karsten <james.e.karsten@gmail.com>
2021-04-17 18:23:59 -0400

    commented and linted python and html files

--- flask_app.py
+++ flask_app.py
@@ -15,50 +15,63 @@ import env
 import base64
 from PIL import Image
 
+# Disables TensorFlow INFO, WARNING, and ERROR messages from being printed
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Creating global variables
 AWS_KEY = env.AWS_KEY
 AWS_SECRET = env.AWS_SECRET
-
-client = boto3.client('sagemaker-runtime', region_name='us-east-1', aws_access_key_id=AWS_KEY, aws_secret_access_key=AWS_SECRET)
-
 UPLOAD_FOLDER = '/home/hilnels/mysite/uploads'
 DOWNLOAD_FOLDER = '/home/hilnels/mysite/downloads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+IMAGE_RESIZE_HEIGHT = 128
+IMAGE_WIDTH_REDUCTION = 16
 CONFIDENCE_THRESHOLD = 0.7
 VOCAB_PATH = '/home/hilnels/mysite/scripts/vocabulary_semantic.txt'
-
 FILENAME = ''
 DOWNLOAD_FILENAME = ''
 
+# TODO: Comment
+client = boto3.client(
+    'sagemaker-runtime',
+    region_name='us-east-1',
+    aws_access_key_id=AWS_KEY,
+    aws_secret_access_key=AWS_SECRET
+)
+
+# TODO: Comment
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def yolo_endpoint_pred(client, endpoint_name, content_type, body):
-    """ inputs: client -- boto3 sagemaker-runtime client
+    ''' inputs: client -- boto3 sagemaker-runtime client
                 endpoint_name -- AWS endpoint name (string)
                 content_type -- image/jpeg for YOLO (string)
                 body -- request body (image bytes)
         outputs: a list of dictionaries representing each label
-    """
-    if content_type != "image/jpeg":
-        return "Wrong content type"
+    '''
+    if content_type != 'image/jpeg':
+        return 'Wrong content type'
 
     ioc_response = client.invoke_endpoint(
         EndpointName=endpoint_name,
         Body=body,
         ContentType=content_type)
 
-    response = ioc_response["Body"].read()
+    response = ioc_response['Body'].read()
     preds = json.loads(response)
     return preds
 
-def split_to_lines(file_name, preds):
-    """ splits a single image of sheet music into multiple images of each line
-        inputs: file_name -- local path to image (string)
-                preds -- list of dictionaries with the keys: x, y, width, height, conf
+
+def split_to_lines(filename, preds):
+    ''' splits a single image of sheet music into multiple images of each line
+        inputs: filename -- local path to image (string)
+                preds -- list of dictionaries with the keys:
+                         x, y, width, height, conf
         outputs: a list of cv2 images
-    """
-    pil_img = Image.open(file_name)
+    '''
+    pil_img = Image.open(filename)
     images = []
     width, height = pil_img.size
     for pred in preds:
@@ -72,21 +85,30 @@ def split_to_lines(file_name, preds):
             images.append(imcv)
     return images
 
+
 def allowed_file(filename):
+    ''' TODO
+    '''
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
 @app.route('/')
 def root():
+    ''' returns the main page when the app is loaded
+        outputs: rendered index HTML page
+    '''
     return render_template('index.html')
 
 
 @app.route('/confirm', methods=['GET', 'POST'])
 def confirm():
+    ''' checks that uploaded image is valid,
+        returns the confirmation page
+        outputs: rendered index HTML page
+    '''
+    # Resets working directory and logs current location
     os.chdir(r'/home/hilnels')
-
     print('confirm:', os.getcwd())
 
     if request.method == 'POST':
@@ -107,45 +129,32 @@ def confirm():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], FILENAME))
             print(FILENAME)
 
-        # return render_template('confirm.html', filename = '/uploads/' + FILENAME)
-        return render_template('confirm.html', filename = FILENAME)
+        return render_template('confirm.html', filename=FILENAME)
 
 
 @app.route('/send_img/<filename>')
 def send_img(filename):
-    '''Displays file on confirmation page
+    ''' returns the uploaded image on the confirmation page
+        inputs: filename -- the file name of the uploaded image
+        outputs: user uploaded image
     '''
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 @app.route('/result', methods=['GET', 'POST'])
 def predict():
-
+    ''' processes the title form input,
+        sends the uploaded image to the 2 neural nets for processing,
+        calls the parser on the semantic output and creates download files,
+        returns the result page with links to download files
+        outputs: rendered index HTML page
+    '''
+    # reset working directory and logs current location
     os.chdir(r'/home/hilnels')
     print('predict:', os.getcwd())
 
     if request.method == 'POST':
-        """
-        print("AWSKEY " + AWS_KEY)
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print("before line 136?")
-            print(filename)
-            # return redirect(url_for('predict', filename=filename))
-        """
-
-        # Checks if user wants to upload a new image
+        # check if user wants to upload a new image
         print(request.form)
         if request.form['button'] == 'Upload different image':
             return render_template('index.html')
@@ -154,7 +163,7 @@ def predict():
         # TODO: check if title is empty/contains invalid characters like . or /
         print(title)
 
-        # Read the dictionary
+        # read the dictionary
         dict_file = open(VOCAB_PATH, 'r')
         dict_list = dict_file.read().splitlines()
         int2word = dict()
@@ -163,14 +172,12 @@ def predict():
             int2word[word_idx] = word
         dict_file.close()
 
-        # Restore weights
-        # TODO: fix magic number
-        width_reduction = 16
+        # restore weights
 
         file_path = UPLOAD_FOLDER + '/' + FILENAME
         print(file_path)
 
-        # Read in file_path
+        # read in file_path
         raw_im = cv2.imread(file_path)
         retval, buffer = cv2.imencode('.jpg', raw_im)
         bytes_jpg = base64.b64encode(buffer)
@@ -182,21 +189,32 @@ def predict():
         split_images = split_to_lines(file_path, preds)
 
         if not split_images:
-            print("NO PREDICTIONS...")
+            print('NO PREDICTIONS...')
 
         # output string
-        predict_to_parse = ""
+        predict_to_parse = ''
 
         for image in split_images:
-            # TODO: magic number
-            image = ctc_utils.resize(image, 128)
+            image = ctc_utils.resize(image, IMAGE_RESIZE_HEIGHT)
             image = ctc_utils.normalize(image)
-            image = np.asarray(image).reshape(1,image.shape[0],image.shape[1],1)
+            image = np.asarray(image).reshape(
+                1,
+                image.shape[0],
+                image.shape[1],
+                1
+            )
             print(image.shape)
 
-            seq_lengths = [ image.shape[2] // width_reduction ]
+            seq_lengths = [image.shape[2] // IMAGE_WIDTH_REDUCTION]
 
-            body = json.dumps({"signature_name": "predict", "inputs": {"input": image.tolist(), "seq_len": seq_lengths, "rnn_keep_prob": 1.0}})
+            body = json.dumps({
+                'signature_name': 'predict',
+                'inputs': {
+                    'input': image.tolist(),
+                    'seq_len': seq_lengths,
+                    'rnn_keep_prob': 1.0
+                }
+            })
 
             ioc_predictor_endpoint_name = 'semantic'
             content_type = 'application/json'
@@ -240,13 +258,18 @@ def predict():
 
 @app.route('/download')
 def download():
-
+    ''' sends download files to user's local machine
+        outputs: output LilyPond file
+    '''
+    # reset working directory and logs current location
     os.chdir(r'/')
-
     print('download:', os.getcwd())
 
-    # return send_from_directory(app.config['DOWNLOAD_FOLDER'], DOWNLOAD_FILENAME, as_attachment=True)
-    return send_file(DOWNLOAD_FOLDER + '/' + DOWNLOAD_FILENAME, as_attachment=True)
+    return send_file(
+        DOWNLOAD_FOLDER + '/' + DOWNLOAD_FILENAME,
+        as_attachment=True
+    )
+
 
 @app.route('/about')
 def about():
--- scripts/sheet_music_parser.py
+++ scripts/sheet_music_parser.py
@@ -1,148 +1,130 @@
-# Test input:
-
-"""clef-C1
-keySignature-EbM
-timeSignature-2/4
-multirest-23
-barline
-rest-quarter
-rest-eighth
-note-Bb4_eighth
-barline
-note-Bb4_quarter.
-note-G4_eighth
-barline
-note-Eb5_quarter.
-note-D5_eighth
-barline
-note-C5_eighth
-note-C5_eighth
-rest-quarter
-barline"""
-
-
-# Dictionary for key signature
+# dictionary of key signatures
 keySigSemanticToLP = {
-    "Cm"  : "\\key c \\minor ",
-    "CM"  : "\\key c \\major ",
-    "C#m" : "\\key cis \\minor ",
-    "C#M" : "\\key cis \\major ",
-    "Cbm" : "\\key ces \\minor ",
-    "CbM" : "\\key ces \\major ",
-    "Dm"  : "\\key d \\minor ",
-    "DM"  : "\\key d \\major ",
-    "D#m" : "\\key dis \\minor ",
-    "D#M" : "\\key dis \\major ",
-    "Dbm" : "\\key des \\minor ",
-    "DbM" : "\\key des \\major ",
-    "Em"  : "\\key e \\minor ",
-    "EM"  : "\\key e \\major ",
-    "E#m" : "\\key eis \\minor ",
-    "E#M" : "\\key eis \\major ",
-    "Ebm" : "\\key ees \\minor ",
-    "EbM" : "\\key ees \\major ",
-    "Fm"  : "\\key f \\minor ",
-    "FM"  : "\\key f \\major ",
-    "F#m" : "\\key fis \\minor ",
-    "F#M" : "\\key fis \\major ",
-    "Fbm" : "\\key fes \\minor ",
-    "FbM" : "\\key fes \\major ",
-    "Gm"  : "\\key g \\minor ",
-    "GM"  : "\\key g \\major ",
-    "G#m" : "\\key gis \\minor ",
-    "G#M" : "\\key gis \\major ",
-    "Gbm" : "\\key ges \\minor ",
-    "GbM" : "\\key ges \\major ",
-    "Am"  : "\\key a \\minor ",
-    "AM"  : "\\key a \\major ",
-    "A#m" : "\\key ais \\minor ",
-    "A#M" : "\\key ais \\major ",
-    "Abm" : "\\key aes \\minor ",
-    "AbM" : "\\key aes \\major ",
-    "Bm"  : "\\key b \\minor ",
-    "BM"  : "\\key b \\major ",
-    "B#m" : "\\key bis \\minor ",
-    "B#M" : "\\key bis \\major ",
-    "Bbm" : "\\key bes \\minor ",
-    "BbM" : "\\key bes \\major "
+    "Cm":   "\\key c \\minor ",
+    "CM":   "\\key c \\major ",
+    "C#m":  "\\key cis \\minor ",
+    "C#M":  "\\key cis \\major ",
+    "Cbm":  "\\key ces \\minor ",
+    "CbM":  "\\key ces \\major ",
+    "Dm":   "\\key d \\minor ",
+    "DM":   "\\key d \\major ",
+    "D#m":  "\\key dis \\minor ",
+    "D#M":  "\\key dis \\major ",
+    "Dbm":  "\\key des \\minor ",
+    "DbM":  "\\key des \\major ",
+    "Em":   "\\key e \\minor ",
+    "EM":   "\\key e \\major ",
+    "E#m":  "\\key eis \\minor ",
+    "E#M":  "\\key eis \\major ",
+    "Ebm":  "\\key ees \\minor ",
+    "EbM":  "\\key ees \\major ",
+    "Fm":   "\\key f \\minor ",
+    "FM":   "\\key f \\major ",
+    "F#m":  "\\key fis \\minor ",
+    "F#M":  "\\key fis \\major ",
+    "Fbm":  "\\key fes \\minor ",
+    "FbM":  "\\key fes \\major ",
+    "Gm":   "\\key g \\minor ",
+    "GM":   "\\key g \\major ",
+    "G#m":  "\\key gis \\minor ",
+    "G#M":  "\\key gis \\major ",
+    "Gbm":  "\\key ges \\minor ",
+    "GbM":  "\\key ges \\major ",
+    "Am":   "\\key a \\minor ",
+    "AM":   "\\key a \\major ",
+    "A#m":  "\\key ais \\minor ",
+    "A#M":  "\\key ais \\major ",
+    "Abm":  "\\key aes \\minor ",
+    "AbM":  "\\key aes \\major ",
+    "Bm":   "\\key b \\minor ",
+    "BM":   "\\key b \\major ",
+    "B#m":  "\\key bis \\minor ",
+    "B#M":  "\\key bis \\major ",
+    "Bbm":  "\\key bes \\minor ",
+    "BbM":  "\\key bes \\major "
 }
 
-# Dictionary for note length
-lengthToNum = { #need to add double dots
-    "quadruple_whole"  : "\\longa ",
-    "quadruple_whole." : "\\longa. ",
-    "double_whole"  : "\\breve ",
-    "double_whole." : "\\breve. ",
-    "whole"        : "1 ",
-    "whole."       : "1. ",
-    "whole.."      : "1.. ",
-    "half"         : "2 ",
-    "half."        : "2. ",
-    "half.."       : "2.. ",
-    "quarter"      : "4 ",
-    "quarter."     : "4. ",
-    "quarter.."    : "4. ",
-    "eighth"       : "8 ",
-    "eighth."      : "8. ",
-    "eighth.."     : "8.. ",
-    "sixteenth"    : "16 ",
-    "sixteenth."   : "16. ",
-    "sixteenth.."  : "16.. ",
-    "thirty_second"   : "32 ",
-    "thirty_second."  : "32. ",
-    "thirty_second.." : "32.. ",
-    "sixty_fourth"  : "64 ",
-    "sixty_fourth." : "64. ",
-    "sixty_fourth.,": "64.. ",
-    "hundred_twenty_eighth" : "128 "
+# dictionary of note lengths
+# need to add double dots
+lengthToNum = {
+    "quadruple_whole":          "\\longa ",
+    "quadruple_whole.":         "\\longa. ",
+    "double_whole":             "\\breve ",
+    "double_whole.":            "\\breve. ",
+    "whole":                    "1 ",
+    "whole.":                   "1. ",
+    "whole..":                  "1.. ",
+    "half":                     "2 ",
+    "half.":                    "2. ",
+    "half..":                   "2.. ",
+    "quarter":                  "4 ",
+    "quarter.":                 "4. ",
+    "quarter..":                "4. ",
+    "eighth":                   "8 ",
+    "eighth.":                  "8. ",
+    "eighth..":                 "8.. ",
+    "sixteenth":                "16 ",
+    "sixteenth.":               "16. ",
+    "sixteenth..":              "16.. ",
+    "thirty_second":            "32 ",
+    "thirty_second.":           "32. ",
+    "thirty_second..":          "32.. ",
+    "sixty_fourth":             "64 ",
+    "sixty_fourth.":            "64. ",
+    "sixty_fourth.,":           "64.. ",
+    "hundred_twenty_eighth":    "128 "
 }
 
-# Dictionary for note pitches
+# dictionary of note pitches
 letterToNote = {
-    "C"  : "c ",
-    "C#" : "cis ",
-    "Cb" : "ces ",
-    "D"  : "d ",
-    "D#" : "dis ",
-    "Db" : "des ",
-    "E"  : "e ",
-    "E#" : "eis ",
-    "Eb" : "ees ",
-    "F"  : "f ",
-    "F#" : "fis ",
-    "Fb" : "fes ",
-    "G"  : "g ",
-    "G#" : "gis ",
-    "Gb" : "ges ",
-    "A"  : "a ",
-    "A#" : "ais ",
-    "Ab" : "aes ",
-    "B"  : "b ",
-    "B#" : "bis ",
-    "Bb" : "bes "
+    "C":   "c ",
+    "C#":  "cis ",
+    "Cb":  "ces ",
+    "D":   "d ",
+    "D#":  "dis ",
+    "Db":  "des ",
+    "E":   "e ",
+    "E#":  "eis ",
+    "Eb":  "ees ",
+    "F":   "f ",
+    "F#":  "fis ",
+    "Fb":  "fes ",
+    "G":   "g ",
+    "G#":  "gis ",
+    "Gb":  "ges ",
+    "A":   "a ",
+    "A#":  "ais ",
+    "Ab":  "aes ",
+    "B":   "b ",
+    "B#":  "bis ",
+    "Bb":  "bes "
 }
 
+# dictionary of clefs
 clefDict = {
-    "C1" : "soprano ",
-    "C2" : "mezzosoprano ",
-    "C3" : "alto ",
-    "C4" : "tenor ",
-    "C5" : "baritone ",
-    "F3" : "baritone ",
-    "F4" : "bass ",
-    "F5" : "subbass ",
-    "G1" : "french ",
-    "G2" : "treble "
+    "C1":   "soprano ",
+    "C2":   "mezzosoprano ",
+    "C3":   "alto ",
+    "C4":   "tenor ",
+    "C5":   "baritone ",
+    "F3":   "baritone ",
+    "F4":   "bass ",
+    "F5":   "subbass ",
+    "G1":   "french ",
+    "G2":   "treble "
 }
 
-beatspermeasure = "1"
+BEATS_PER_MEASURE = "1"
 
 
 def parser(toparse):
-    """Parses each command output of the semantic model. Returns the equivalent LilyPond command
+    """ parses each command output of the semantic model,
+        returns the equivalent LilyPond command
+        inputs: toparse -- TODO
+        outputs: TODO
     """
     divided_string = toparse.split("-")
-    global beatspermeasure
+    global BEATS_PER_MEASURE
 
     if divided_string[0] == "clef":
         return "\\clef " + clefDict[divided_string[1]] + "\n"
@@ -151,7 +133,7 @@ def parser(toparse):
         return keySigSemanticToLP[divided_string[1]] + "\n"
 
     elif divided_string[0] == "timeSignature":
-        beatspermeasure = divided_string[1][0]
+        BEATS_PER_MEASURE = divided_string[1][0]
         if divided_string[1] == "C":
             return "\\time 4/4 \n"
         elif divided_string[1] == "C/":
@@ -161,10 +143,12 @@ def parser(toparse):
 
     # this is the number of beats, so somehow we need to keep track of the time signature
     elif divided_string[0] == "multirest":
-        if beatspermeasure != 0:
-            return "\\compressFullBarRests \n \t R1*" + str(int(divided_string[1])//int(beatspermeasure)) + "\n "
+        if BEATS_PER_MEASURE != 0:
+            return "\\compressFullBarRests \n \t R1*" +\
+                str(int(divided_string[1])//int(BEATS_PER_MEASURE)) + "\n "
         else:
-            return "\\compressFullBarRests \n \t R1*" + divided_string[1] + "\n "
+            return "\\compressFullBarRests \n \t R1*" +\
+                divided_string[1] + "\n "
 
     elif divided_string[0] == "barline":
         return " \\bar \"|\" "
@@ -184,20 +168,28 @@ def parser(toparse):
             ending = "\\fermata "
             note_info[1] = note_info[1][:-8]
         if int(note_info[0][-1]) == 3:
-            return letterToNote[note_info[0][:-1]] + lengthToNum[note_info[1]] + ending
+            return letterToNote[note_info[0][:-1]] +\
+                lengthToNum[note_info[1]] + ending
         elif int(note_info[0][-1]) < 3:
-            return letterToNote[note_info[0][:-1]] + (3 - int(note_info[0][-1])) * "," + lengthToNum[note_info[1]] + ending
+            return letterToNote[note_info[0][:-1]] +\
+                (3 - int(note_info[0][-1])) * "," +\
+                lengthToNum[note_info[1]] + ending
         elif int(note_info[0][-1]) > 3:
-            return letterToNote[note_info[0][:-1]] + (int(note_info[0][-1]) - 3) * "\'" + lengthToNum[note_info[1]] + ending
+            return letterToNote[note_info[0][:-1]] +\
+                (int(note_info[0][-1]) - 3) * "\'" +\
+                lengthToNum[note_info[1]] + ending
 
     elif divided_string[0] == "gracenote":
         note_info = divided_string[1].split("_", 1)
         if int(note_info[0][-1]) == 3:
-            notepart = letterToNote[note_info[0][:-1]] + lengthToNum[note_info[1]]
+            notepart = letterToNote[note_info[0][:-1]] +\
+                lengthToNum[note_info[1]]
         elif int(note_info[0][-1]) < 3:
-            notepart =  letterToNote[note_info[0][:-1]] + (3 - int(note_info[0][-1])) * "," + lengthToNum[note_info[1]]
+            notepart = letterToNote[note_info[0][:-1]] +\
+                (3 - int(note_info[0][-1])) * "," + lengthToNum[note_info[1]]
         elif int(note_info[0][-1]) > 3:
-            notepart = letterToNote[note_info[0][:-1]] + (int(note_info[0][-1]) - 3) * "\'" + lengthToNum[note_info[1]]
+            notepart = letterToNote[note_info[0][:-1]] +\
+                (int(note_info[0][-1]) - 3) * "\'" + lengthToNum[note_info[1]]
         return "\\grace { " + notepart + " }"
 
     elif divided_string[0] == "tie":
@@ -208,13 +200,16 @@ def parser(toparse):
 
 
 def generate_music(model_output, piece_title):
-    """generate_music calls the parser to parse the input and sends it to a LilyPond file to generate a PDF
-       Model_output is the string output produced by the semantic model, unedited
-       Piece_title provides the title of the piece, which becomes the filename
+    """ calls the parser to parse the input,
+        sends it to a LilyPond file to generate a PDF
+        inputs: model_output -- string output produced by the semantic model
+                piece_title -- title of the piece, which becomes the filename
+        outputs: TODO
     """
     element_list = model_output.split("\n")
 
-    lilyPond = "\\version \"2.20.0\" \n\header{\n  title = \"" + piece_title + "\"\n}\n{\n"
+    lilyPond = "\\version \"2.20.0\" \n\header{\n  title = \"" +\
+        piece_title + "\"\n}\n{\n"
 
     # f = open(piece_title + ".ly", "w+")
     # f.write("\\version \"2.20.0\" \n\header{\n  title = \"" + piece_title + "\"\n}\n{\n")
